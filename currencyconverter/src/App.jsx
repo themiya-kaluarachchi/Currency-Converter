3f13b36f132955f5f703044dd8299cdf4b4cbcd5
Themiya <themiyakaluarachchi@gmail.com>
2025-06-08 06:50:16 +0530

    add feat to inputBox

--- currencyconverter/src/App.jsx
+++ currencyconverter/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
 
   return (
     <>
-     <h1 className='bg-red-200'>Test for tailwind</h1>
+     <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url(https://images.pexels.com/photos/8442322/pexels-photo-8442322.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+
+      <h1 className='bg-red-200'>Test for tailwind</h1>
+     </div>
+     
     </>
   )
 }
